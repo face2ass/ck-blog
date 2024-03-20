6307a42a12f4153b18a625cdc4ea4c67811b82e9
Kay <just_be_fine@163.com>
2024-03-21 01:41:38 +0800

    主题优化

--- themes/kaysama/source/js/script.js
+++ themes/kaysama/source/js/script.js
@@ -1,11 +1,20 @@
-(function($){
-  const $blogPostContent = $('#blog-post-content')
-  if($blogPostContent.length) {
-    const $blockquote = $blogPostContent.find('blockquote')
-    if($blockquote.length) {
-      $blockquote.each((index, ele) => {
-        $(ele).addClass('blockquote')
-      })
-    }
+(function ($) {
+  // const $blogPostContent = $('#blog-post-content')
+  // if ($blogPostContent.length) {
+  //   const $blockquote = $blogPostContent.find('blockquote')
+  //   if ($blockquote.length) {
+  //     $blockquote.each((index, ele) => {
+  //       $(ele).addClass('blockquote')
+  //     })
+  //   }
+  // }
+  const $highlight = $('.highlight')
+  if ($highlight.length) {
+    $highlight.each((index, ele) => {
+      $(ele).addClass('bg-body-tertiary border rounded')
+    })
   }
-})(jQuery);
+
+  // data-bs-theme="dark"
+  // document.documentElement
+})(jQuery)
